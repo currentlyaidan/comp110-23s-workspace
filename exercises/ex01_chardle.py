f6f0bfe27274238b3e04d5749acdece2835ec587
Aidan yung <aidany@ad.unc.edu>
2023-09-05 15:52:25 -0400

    Created chardle program

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,8 @@
+"""EX01 - A cute step towards Wordle."""
+
+__author__ = "730679404"
+
+five_letter_word: str = input("Enter a 5 character word: ")
+single_character: str = input("Enter a single character: ")
+
+print("Searching for " + str(single_character) + " in " + str(five_letter_word))
