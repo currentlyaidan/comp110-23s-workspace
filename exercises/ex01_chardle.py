5f13f066ac1f28dde73420c9778ec5004c07e216
Aidan yung <aidany@ad.unc.edu>
2023-09-06 22:03:16 -0400

    Finished ex01 assignment

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,10 +3,15 @@
 __author__ = "730679404"
 
 five_letter_word: str = input("Enter a 5 character word: ")
-if len(five_letter_word) == 5:
-    
+if len(five_letter_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
 
 single_character: str = input("Enter a single character: ")
+if len(single_character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+
 matching_letters: int = 0
 
 print("Searching for " + str(single_character) + " in " + str(five_letter_word))
@@ -32,3 +37,4 @@ if matching_letters == 1:
     print(str(matching_letters) + " instance of " + str(single_character) + " found in " + str(five_letter_word))
 if matching_letters > 1:
     print(str(matching_letters) + " instances of " + str(single_character) + " found in " + str(five_letter_word))
+
