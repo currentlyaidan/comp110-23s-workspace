dc053adea1a4eee0a2e4cb623d21f61afb9d3dfa
Aidan yung <aidany@ad.unc.edu>
2023-09-17 00:02:31 -0400

    Final program that works

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,8 +2,9 @@
 
 __author__: str = 730679404
 
+#set all variables to initial values
 secret_word: str = "python"
-word_guess: str = input("What is your 6-letter guess? ")
+word_guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 index_number: int = 0
 printed_string: str = ""
 WHITE_BOX: str = "\U00002B1C"
@@ -12,27 +13,35 @@ YELLOW_BOX: str = "\U0001F7E8"
 found_letter: bool = False
 check_all: int = 0
 
-while len(word_guess) != 6:
-    word_guess = input("That was not 6 letters! Try again: ")
+#detect for improper inputs and prompt repeatedly until user inputs valid input
+while len(word_guess) != len(secret_word):
+    word_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
+#detect if the letter is the same in each index
 while index_number < len(word_guess):
+    found_letter = False
     check_all = 0
+    #if the letter is the same print green
     if word_guess[index_number] == secret_word[index_number]:
         printed_string += (GREEN_BOX)
         index_number += 1
     else:
-        while not(found_letter == False) and (check_all < len(word_guess)):
+        #check if the letter exists anywhere in the word
+        while not(found_letter == True) and (check_all < len(word_guess)):
             if secret_word[check_all] == word_guess[index_number]:
                 found_letter = True
             else:
                 check_all += 1
-    if (found_letter == True):
-        printed_string += (YELLOW_BOX)
-        index_number += 1
-    else:
-        printed_string += (WHITE_BOX)
-        index_number += 1
-
+        #if the letter exists elsewhere in the word, print yellow
+        if not(found_letter == False):
+            printed_string += (YELLOW_BOX)
+            index_number += 1
+        #otherwise, print white
+        else:
+            printed_string += (WHITE_BOX)
+            index_number += 1
+            
+#output final string of boxes, as well as a success or failure message
 if word_guess == secret_word:
     print(printed_string)
     print("Woo! You got it!")
