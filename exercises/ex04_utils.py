81377316da43ebf3417e1c28dd83b705f06551ca
Aidan yung <aidany@ad.unc.edu>
2023-10-08 20:53:04 -0400

    formatting for autograder

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,10 +2,13 @@
 
 __author__ = "730679404"
 
+
 def all(nums: list[int], num: int) -> bool:
     """Returns True if all numbers match, returns False if they don't."""
     checker: int = 0
     # Check each number to see if they match.
+    if len(nums) == 0:
+        return False
     while (checker < len(nums)):
         if nums[checker] == num:
             checker += 1
@@ -13,19 +16,21 @@ def all(nums: list[int], num: int) -> bool:
             return False
     return True
 
+
 def max(nums: list[int]) -> int:
-    """Returns the largest value in a list of integers"""
-    largest: int = 0
-    checker: int = 0
+    """Returns the largest value in a list of integers."""
+    checker: int = 1
     # Check if the list is empty, in which case print an error.
     if len(nums) == 0:
         raise ValueError("max() arg is an empty List")
+    largest: int = nums[0]
     # Check each index on the list to figure out if the number is the largest.
     while (checker < len(nums)):
         if nums[checker] > largest:
             largest = nums[checker]
         checker += 1
     return largest
+
 
 def is_equal(ints_one: list[int], ints_two: list[int]) -> bool:
     """Checks to see if each corresponding value in two lists are equal, returns False if not."""
