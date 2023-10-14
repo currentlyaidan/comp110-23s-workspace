38b587f8d5b3469e52af03c342ae695458e83e55
Aidan yung <aidany@ad.unc.edu>
2023-10-14 16:34:11 -0400

    finished tutorial

--- lessons/turtle_tutorial.py
+++ lessons/turtle_tutorial.py
@@ -10,13 +10,33 @@ leo.penup()
 leo.goto(-150,-30)
 leo.pendown()
 leo.fillcolor("pink")
-leo.color("black","pink")
+leo.color("black",(0,150,72))
 
+leo.speed(70)
+leo.hideturtle()
 leo.begin_fill()
-while i < 4:
+while i < 3:
     leo.forward(300)
     leo.left(120)
     i += 1
 leo.end_fill()
+
+bob: Turtle = Turtle()
+
+bob.color("black")
+bob.penup()
+bob.goto(-150,-30)
+bob.pendown()
+bob.speed(100)
+
+g: int = 0
+side_length: int = 300
+while side_length > 1:
+    g = 0
+    while g < 3:
+        bob.forward(side_length)
+        bob.left(120.3)
+        g += 1
+    side_length *= 0.97
 
 done()
