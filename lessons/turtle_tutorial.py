d746d78b5869bcc36c0cd7e7c2c1b62ab34d627f
Aidan yung <aidany@ad.unc.edu>
2023-10-14 16:11:27 -0400

    finished part 2

--- lessons/turtle_tutorial.py
+++ lessons/turtle_tutorial.py
@@ -1,11 +1,22 @@
 """Turtle Tutorial."""
 
 from turtle import Turtle, colormode, done
+colormode(255)
 leo: Turtle = Turtle()
 i: int = 0
+
+leo.pencolor(252,240,39)
+leo.penup()
+leo.goto(-150,-30)
+leo.pendown()
+leo.fillcolor("pink")
+leo.color("black","pink")
+
+leo.begin_fill()
 while i < 4:
     leo.forward(300)
-    leo.left(90)
+    leo.left(120)
     i += 1
+leo.end_fill()
 
 done()
