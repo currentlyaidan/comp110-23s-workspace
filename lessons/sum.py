ea3af3281724cd972e606bcda382bd390f9899a9
Aidan yung <aidany@ad.unc.edu>
2023-10-17 11:59:59 -0400

    challenge question done

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,27 +2,28 @@
 
 __author__ = "730679404"
 
-vals: list[float] = [1.1, 0.9, 1.0]
 
-def w_sum(nums = list[float]) -> float:
+def w_sum(vals: list[float]) -> float: 
     """Uses a while loop."""
     idx: int = 0
     sum: float = 0
-    while idx<len(list):
-        sum += list[idx]
+    while idx < len(vals):
+        sum += vals[idx]
         idx += 1
     return sum
 
-def f_sum(nums = list[float]) -> float:
+
+def f_sum(vals: list[float]) -> float: 
     """Uses a for/in loop."""
     sum: float = 0
-    for value in nums:
+    for value in vals: 
         sum += value
     return sum
 
-def f_range_sum(nums = list[float]) -> float:
+
+def f_range_sum(vals: list[float]) -> float: 
     """Uses a for/in range loop."""
     sum: float = 0
-    for value in range(0,len(vals)):
-        sum += value
+    for value in range(0, len(vals)): 
+        sum += vals[value]
     return sum
