8189a2cf5592f4c6643ec978a7b12031ad9017bb
Aidan yung <aidany@ad.unc.edu>
2023-10-16 21:33:30 -0400

    Started project

---
+++ exercises/turtle_project.py
@@ -0,0 +1,21 @@
+"""'I want you more than any blue sky!' Update later with lines for attempted points"""
+
+__author__ = "730679404"
+
+from turtle import Turtle, colormode, done
+colormode(255)
+
+def main() -> None:
+    """The entrypoint of my scene."""
+    # Declare Turtle variables
+    city: Turtle = Turtle()
+    # Call procedures which I define and use my turtles as arguments
+    done()
+
+# Define the procedures for other components    
+def land() -> None:
+    """Draws the land."""
+    land: Turtle = Turtle()
+    land.color((237,223,192),(117,167,117))
+
+# __name__ is "__main__" command
