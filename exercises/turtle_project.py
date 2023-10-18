f369430ab48c5e5903d9d73df16b72418065dd0e
Aidan yung <aidany@ad.unc.edu>
2023-10-18 18:20:23 -0400

    Pantssss

--- exercises/turtle_project.py
+++ exercises/turtle_project.py
@@ -11,36 +11,69 @@ screen.setup(width=1.0, height=1.0, startx=0, starty=1)
 def main() -> None:
     """The entrypoint of my scene."""
     # Declare Turtle variables
-    pants: Turtle = Turtle()
     # Call procedures which I define and use my turtles as arguments
     land()
     water()
-
-
+    hinapants()
     repeat: int = 0
     while repeat < 150:
         city(randint(-733,720),randint(-400,0),randint(10,100),randint(5,50))
+        repeat += 1
     done()
 
 
+def prepare_pen(x: int, y: int,turt: Turtle) -> None:
+    """Gets the pen in the right spot, saves lines of code, and prepares to draw."""
+    turt.hideturtle()
+    turt.speed(0)
+    turt.penup()
+    turt.goto(x,y)
+    turt.pendown()
+
+
 def draw_to(x = int, y = int, turt = Turtle) -> None:
-    """Draws a line to the given coordinates"""
+    """Draws a line to the given coordinates."""
     turt.pendown
     turt.speed(0)
     turt.hideturtle()
     turt.goto(x,y)
 
 
+def hinapants() -> None:
+    """Draws Hina's pants."""
+    pants: Turtle = Turtle()
+    prepare_pen(-300,410,pants)
+    pants.color((0,0,0),(176,136,254))
+    pants.begin_fill()
+    pants.goto(-300,375)
+    pants.goto(-298,360)
+    pants.goto(-260,356)
+    pants.goto(-210,358)
+    pants.goto(-170,364)
+    pants.goto(-140,370)
+    pants.goto(-137,410)
+    pants.goto(-300,410)
+    pants.end_fill()
+    pants.penup()
+    pants.goto(-170,370)
+    pants.pendown()
+    pants.goto(-205,380)
+    pants.goto(-215,377)
+
+def hinashirt() -> None:
+    """Draws Hina's shirt."""
+    shirt: Turtle() = Turtle
+    prepare_pen(-300,375,shirt)
+    shirt.color((0,0,0),(218,136,254))
+    
+
+
 # Define the procedures for other components    
 def water() -> None:
     """Draws the water."""
     water: Turtle = Turtle()
-    water.hideturtle()
     water.color((101,152,193),(101,152,193))
-    water.penup()
-    water.goto(-733,0)
-    water.pendown()
-    water.speed(0)
+    prepare_pen(-733,0,water)
     water.begin_fill()
     draw_to(732,0,water)
     draw_to(732,100,water)
@@ -68,12 +101,8 @@ def water() -> None:
 def land() -> None:
     """Draws the land."""
     land: Turtle = Turtle()
-    land.hideturtle()
     land.color((237,223,192),(117,167,117))
-    land.penup()
-    land.goto(-733,0)
-    land.pendown()
-    land.speed(0)
+    prepare_pen(-733,0,land)
     # Make land base
     land.begin_fill()
     draw_to(732,0,land)
@@ -102,10 +131,7 @@ def city(x: int, y: int,height: int, width: int) -> None:
     city.hideturtle()
     color: int = randint(75,155)
     city.color((color,color,color),(color,color,color))
-    city.penup()
-    city.goto(x,y)
-    city.pendown()
-    city.speed(0)
+    prepare_pen(x,y,city)
     city.begin_fill()
     city.seth(330)
     city.forward(width)
