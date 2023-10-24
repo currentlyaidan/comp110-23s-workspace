e951f78d524a785e1350076afec34004e8fdeade
Aidan yung <aidany@ad.unc.edu>
2023-10-24 11:57:23 -0400

    started zip

---
+++ lessons/zip.py
@@ -0,0 +1,7 @@
+"""Combining two lists into a dictionary."""
+
+__author__ = "730679404"
+
+def zip(first: list[str], second: list[int]) -> dict[str,int]:
+    """Combine two lists into a dictionary."""
+    
