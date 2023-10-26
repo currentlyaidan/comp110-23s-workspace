a42069e11a328fdcc4df16e3c1bdb3b8e7d338d7
Aidan yung <aidany@ad.unc.edu>
2023-10-26 11:12:35 -0400

    created

---
+++ lessons/sum_evens.py
@@ -0,0 +1,7 @@
+"""Practice summing over lists."""
+
+
+def sum_evens_of_list(input_list: list[int]) -> int:
+    """Loop over a list and sum all even elements."""
+    sum_total: int = 0
+    return sum_total
---
+++ lessons/sum_evens_test.py
@@ -0,0 +1,12 @@
+"""Testing my sum of evens function."""
+
+from lessons.sum_evens import sum_evens_of_list
+
+def test_empty_list() -> None:
+    """sum_evens_of_list([]) should return 0"""
+    assert sum_evens_of_list([]) == 0
+
+def test_sum_one_element() -> None:
+    """sum_evens_of_list([2]) = 2"""
+    test_list: list[int] = 2
+    assert sum_evens_of_list(test_list) == 2
