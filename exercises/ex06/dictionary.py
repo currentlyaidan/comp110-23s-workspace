c5cbebc6d6b606bd73fdcf4c7f8c3c156cc54c96
Aidan yung <aidany@ad.unc.edu>
2023-10-26 12:03:44 -0400

    started

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,7 @@
+"""EX06 Dictionary"""
+
+def invert(inverse: dict[str, str]) -> dict[str, str]:
+    """Inverts the two values in a list."""
+    idx: int = 0
+    inverted: dict[str, str] = {}
+    
