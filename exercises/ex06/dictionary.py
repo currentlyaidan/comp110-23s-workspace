8a0748b3307bcfcfc691107270fa51cb3f1e12e2
Aidan yung <aidany@ad.unc.edu>
2023-10-28 22:32:22 -0400

    wip

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -4,4 +4,16 @@ def invert(inverse: dict[str, str]) -> dict[str, str]:
     """Inverts the two values in a list."""
     idx: int = 0
     inverted: dict[str, str] = {}
-    
+    for key in inverse:
+        if inverse[key] in inverted:
+            raise KeyError("KeyError")
+        inverted[inverse[idx]] = key
+        idx += 1
+    return inverted
+
+
+def favorite_color(pref: dict[str, str]) -> str:
+    """Returns the most popular color."""
+    invert(pref)
+    idx: int = 0
+    if pref[idx] in pref:
