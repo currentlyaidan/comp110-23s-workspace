683bba0359f62581639ecc7c84a7a339a477e40e
Aidan yung <aidany@ad.unc.edu>
2023-11-02 11:59:59 -0400

    class nov 2

---
+++ lessons/CQ07/point.py
@@ -0,0 +1,26 @@
+"""CQ07."""
+
+from __future__ import annotations
+
+class Point:
+    """Point on an x and y plane."""
+    x : float
+    y: float
+
+
+    def __init__(self, x_init: float, y_init: float):
+        """Assign parameters."""
+        self.x = x_init
+        self.y = y_init
+
+
+    def scale_by(self, factor: int):
+        """Scale the point by a factor."""
+        self.x *= factor
+        self.y *= factor
+
+
+    def scale(self, factor: int) -> Point:
+        """Create a new point a factor away from the old point."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return Point
