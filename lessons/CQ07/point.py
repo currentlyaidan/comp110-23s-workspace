99c64d541c9715b74049a14c3cc49f82a6fcd012
Aidan yung <aidany@ad.unc.edu>
2023-11-02 18:04:10 -0400

    point

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -2,25 +2,23 @@
 
 from __future__ import annotations
 
+
 class Point:
     """Point on an x and y plane."""
-    x : float
+    x: float
     y: float
-
 
     def __init__(self, x_init: float, y_init: float):
         """Assign parameters."""
         self.x = x_init
         self.y = y_init
 
-
-    def scale_by(self, factor: int):
+    def scale_by(self, factor: int) -> None:
         """Scale the point by a factor."""
         self.x *= factor
         self.y *= factor
 
-
     def scale(self, factor: int) -> Point:
         """Create a new point a factor away from the old point."""
         new_point: Point = Point(self.x * factor, self.y * factor)
-        return Point
+        return new_point
